a5fae4d09d0b724f4b38eb3fcdc32cbe3f4fde5d
we684123 <we684123@gmail.com>
2020-07-11 19:40:55 +0800

    小改 gs test

--- build/gs_test.ts
+++ build/gs_test.ts
@@ -1,7 +1,9 @@
 import Logger from './Logger.ts'
+
+
+
 function t1() {
   console.log("---------------------");
-
   let logger = new Logger()
   logger.set_GMT('GMT+8')
   logger.set_sheet_id("1lqlqztKroBwDZ--VxoYN9Hh_BuwOzbdbowltI7yf2N4")
@@ -9,6 +11,7 @@ function t1() {
   logger.set_use_sheet(true)
 
   console.log(logger.get_config())
+
   let levels = logger.levels
   logger.log(levels.CRITICAL, '這是測試 CRITICAL')
   logger.log(levels.ERROR, '這是測試 ERROR')
@@ -22,5 +25,24 @@ function t1() {
   logger.debug('這是測試 DEBUG 2')
   logger.info('這是測試 INFO 2')
   logger.notest('這是測試 NOTSET 2')
+}
 
+function t2() {
+  console.log("---------------------");
+  let logger = new Logger()
+  console.log(logger.get_config())
+
+  let levels = logger.levels
+  logger.log(levels.CRITICAL, 'logger.log這是測試 CRITICAL');
+  logger.log(levels.ERROR, 'logger.log這是測試 ERROR');
+  logger.log(levels.WARNING, 'logger.log這是測試 WARNING');
+  logger.log(levels.DEBUG, 'logger.log這是測試 DEBUG');
+  logger.log(levels.INFO, 'logger.log這是測試 INFO');
+  logger.log(levels.NOTSET, 'logger.log這是測試 NOTSET');
+  logger.critical('這是測試 CRITICAL 2')
+  logger.error('這是測試 ERROR 2')
+  logger.warning('這是測試 WARNING 2')
+  logger.debug('這是測試 DEBUG 2')
+  logger.info('這是測試 INFO 2')
+  logger.notest('這是測試 NOTSET 2')
 }
